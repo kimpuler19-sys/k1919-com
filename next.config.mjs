1811cc9760d1c26eaf3d0cddea74a659eeccf05b
jk_kimpuler <admin@kimpler.com>
2026-05-17 10:40:04 +0700

    9

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  
   reactCompiler: true,
+
+  // Tambahkan fungsi pengalihan URL ke file HTML di bawah ini:
+  async redirects() {
+    return [
+      {
+        source: '/joko-nardi',
+        destination: '/jokonardi.html',
+        permanent: true,
+      },
+      {
+        source: '/jokonardi',
+        destination: '/jokonardi.html',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
